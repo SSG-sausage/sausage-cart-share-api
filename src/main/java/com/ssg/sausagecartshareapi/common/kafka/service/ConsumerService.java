3cea0b695f567aea276cafbcc2da7442e70a05d8
Joon <orijoon98@gmail.com>
2023-01-27 12:29:47 +0900

    [TERK-121/Feat] 공유장바구니 아이템 삭제 이벤트 Consumer 로직 추가

--- src/main/java/com/ssg/sausagecartshareapi/common/kafka/service/ConsumerService.java
+++ src/main/java/com/ssg/sausagecartshareapi/common/kafka/service/ConsumerService.java
@@ -3,11 +3,18 @@ package com.ssg.sausagecartshareapi.common.kafka.service;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ssg.sausagecartshareapi.cartshare.dto.CartShareUpdateDto;
 import com.ssg.sausagecartshareapi.cartshare.entity.CartShare;
+import com.ssg.sausagecartshareapi.cartshare.entity.CartShareItem;
+import com.ssg.sausagecartshareapi.cartshare.entity.CartShareMbr;
+import com.ssg.sausagecartshareapi.cartshare.entity.ProgStatCd;
+import com.ssg.sausagecartshareapi.cartshare.repository.CartShareItemRepository;
+import com.ssg.sausagecartshareapi.cartshare.repository.CartShareMbrRepository;
 import com.ssg.sausagecartshareapi.cartshare.service.CartShareUtilService;
 import com.ssg.sausagecartshareapi.common.exception.ErrorCode;
 import com.ssg.sausagecartshareapi.common.exception.InternalServerException;
 import com.ssg.sausagecartshareapi.common.kafka.constant.KafkaConstants;
+import com.ssg.sausagecartshareapi.common.kafka.dto.CartShareItemDeleteListDto;
 import com.ssg.sausagecartshareapi.common.kafka.dto.CartShareUpdateEditPsblYnDto;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
@@ -18,6 +25,8 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class ConsumerService {
 
+    private final CartShareMbrRepository cartShareMbrRepository;
+    private final CartShareItemRepository cartShareItemRepository;
     private final CartShareUtilService cartShareUtilService;
     private final ObjectMapper objectMapper;
     private final SimpMessagingTemplate simpMessagingTemplate;
@@ -29,6 +38,24 @@ public class ConsumerService {
             CartShareUpdateEditPsblYnDto dto = objectMapper.readValue(message, CartShareUpdateEditPsblYnDto.class);
             CartShare cartShare = cartShareUtilService.findCartShareById(dto.getCartShareId());
             cartShare.updateEditPsblYn(dto.getEditPsblYn());
+            simpMessagingTemplate.convertAndSend(
+                    "/sub/cart-share/" + dto.getCartShareId(),
+                    CartShareUpdateDto.of(dto.getCartShareId(), 0L, "update"));
+        } catch (Exception exception) {
+            throw new InternalServerException("예상치 못한 서버 에러가 발생하였습니다.", ErrorCode.INTERNAL_SERVER_EXCEPTION);
+        }
+    }
+
+    @Transactional
+    @KafkaListener(topics = KafkaConstants.KAFKA_CART_SHARE_ITEM_DELETE, groupId = KafkaConstants.CONSUMER_GROUP_ID)
+    public void deleteCartShareItem(String message) {
+        try {
+            CartShareItemDeleteListDto dto = objectMapper.readValue(message, CartShareItemDeleteListDto.class);
+            CartShare cartShare = cartShareUtilService.findCartShareById(dto.getCartShareId());
+            List<CartShareItem> cartShareItemList = cartShareItemRepository.findAllByCartShare(cartShare);
+            List<CartShareMbr> cartShareMbrList = cartShareMbrRepository.findAllByCartShare(cartShare);
+            cartShareMbrList.forEach(cartShareMbr -> cartShareMbr.updateProgStatCd(ProgStatCd.IN_PROGRESS));
+            cartShareItemRepository.deleteAllInBatch(cartShareItemList);
             simpMessagingTemplate.convertAndSend(
                     "/sub/cart-share/" + dto.getCartShareId(),
                     CartShareUpdateDto.of(dto.getCartShareId(), 0L, "update"));
